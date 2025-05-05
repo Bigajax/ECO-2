6d4d637a483d129ccafc5e4747ec387bb300e3d1
Bigajax <rafaelr.barbon@gmail.com>
2025-05-05 00:52:43 -0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path'; // Importe o módulo 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      'components': path.resolve(__dirname, 'src/components'),
+      'screens': path.resolve(__dirname, 'src/screens'),
+    },
   },
 });
