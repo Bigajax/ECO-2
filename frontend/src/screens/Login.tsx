f6baadf46f4d3111faccca7f8e9ebfd3e4825345
Bigajax <rafaelr.barbon@gmail.com>
2025-05-05 00:52:40 -0300

    Update Login.tsx

--- frontend/src/screens/Login.tsx
+++ frontend/src/screens/Login.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import Logo from './../../frontend/src/components/Logo';
+import Logo from 'components/Logo';
 import PageTransition from '../../components/PageTransition';
 
 const Login: React.FC = () => {
